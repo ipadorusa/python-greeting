ed1cb7a2c66b1913a51647cfcc5cba04cf0b9d42
Kimwoosuk <ipadorusa@gmail.com>
2021-02-17 14:48:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 
 from selenium import webdriver
 
-
-
-options = webdriver.ChromeOptions()
-options.add_argument("start-maximized")
-options.add_argument("lang=ko_KR")
-options.add_argument('headless')
-options.add_argument('window-size=1920x1080')
-options.add_argument("disable-gpu")
-options.add_argument("--no-sandbox")
-
 # chrome driver
-driver = webdriver.Chrome('chromedriver', chrome_options=options)
+driver = webdriver.Chrome('chromedriver')
 driver.implicitly_wait(3)
 
 driver.get('https://www.greating.co.kr/login/login?reurl=https%3A%2F%2Fwww.greating.co.kr%2Findex#')
