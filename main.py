ff64c77d9e9f2986b0d5eff2cb335831f71c4b10
Kimwoosuk <ipadorusa@gmail.com>
2021-02-20 10:08:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-
 from selenium import webdriver
 
-options = webdriver.ChromeOptions()
-options.add_argument("start-maximized")
-options.add_argument("lang=ko_KR")
-options.add_argument('headless')
-options.add_argument('window-size=1920x1080')
-options.add_argument("disable-gpu")
-options.add_argument("--no-sandbox")
-
-# chrome driver
-driver = webdriver.Chrome('chromedriver', chrome_options=options)
+driver = webdriver.Chrome('chromedriver')
 driver.implicitly_wait(3)
 
 driver.get('https://www.greating.co.kr/login/login?reurl=https%3A%2F%2Fwww.greating.co.kr%2Findex#')
@@ -23,6 +13,3 @@ driver.implicitly_wait(3)
 driver.get('https://www.greating.co.kr/event/eventCheckAttendance?evEventId=2978#page1')
 submit = driver.find_element_by_id('btnCheck')
 submit.click()
-
-driver.implicitly_wait(5)
-driver.quit()
