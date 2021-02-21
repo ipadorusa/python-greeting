462200dd8558338c6bbedea2fd2eca914e6f8cc4
Kimwoosuk <ipadorusa@gmail.com>
2021-02-21 11:13:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import os.path
 from selenium import webdriver
 
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')               # headless
-# options.add_argument('--no-sandbox')
-# options.add_argument('--disable-dev-shm-usage')
-# options.add_argument('--disable-gpu')
+options.add_argument('headless')
 
-driver_path = os.path.join(CURRENT_DIR, './chromedriver')
-driver = webdriver.Chrome(driver_path, options=options)
+# chrome driver
+driver = webdriver.Chrome('chromedriver', options=options)
 driver.implicitly_wait(3)
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")	options.add_argument("start-maximized")
+options.add_argument("lang=ko_KR")	options.add_argument("lang=ko_KR")
+options.add_argument('headless')	options.add_argument('headless')
+options.add_argument('window-size=1920x1080')	options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")	options.add_argument("disable-gpu")
+options.add_argument("--no-sandbox")	options.add_argument("--no-sandbox")
 
 driver.get('https://www.greating.co.kr/login/login?reurl=https%3A%2F%2Fwww.greating.co.kr%2Findex#')
 driver.maximize_window()
