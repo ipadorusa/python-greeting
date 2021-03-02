968c7f6909d50384b2e94668c7e1654366633147
Kimwoosuk <ipadorusa@gmail.com>
2021-03-02 18:20:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ options.add_argument('headless')
 # chrome driver
 driver = webdriver.Chrome('chromedriver', options=options)
 driver.implicitly_wait(3)
-driver.get('https://www.greating.co.kr/login/login?reurl=https%3A%2F%2Fwww.greating.co.kr%2Findex#')
+driver.get('https://www.greating.co.kr/event/eventCheckAttendance?evEventId=3178#page1')
+driver.find_element_by_id('btnCheck').click();
 driver.implicitly_wait(3)
+driver.find_element_by_id('dvConfirmOk').click();
 driver.find_element_by_xpath('//*[@id="contents"]/div/div[1]/ul[1]/li[2]/a').click()
 driver.find_element_by_id('id').send_keys('ipadorusa')
 driver.find_element_by_id('pw').send_keys('6_u8!v9g2dq')
 driver.find_element_by_id('loginBtn').click()
 driver.implicitly_wait(3)
 driver.get('https://www.greating.co.kr/event/eventCheckAttendance?evEventId=2978#page1')
-submit = driver.find_element_by_id('btnCheck')
-submit.click()
+driver.find_element_by_id('btnCheck').click()
